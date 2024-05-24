8461c4949b5994ac0bb87c88b244fc6ed18533ce
Vlad-Robert Baciu <robertvladbaciu@gmail.com>
2024-05-24 16:56:59 +0300

    fixes #1; Use absolute pathing for `express.static(path)` call

--- server/expressApp/expressApp.js
+++ server/expressApp/expressApp.js
@@ -1,12 +1,15 @@
 'use strict';
 
 import express from 'express';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 import { assignRequestId } from './middlewares/requestIdAssigner.js';
 import { homeRoute } from './routes/homeRouter.js';
 
 export function expressInit(app) {
-    app.use('/', express.static('./expressApp/static'));
+    const directoryName = dirname(fileURLToPath(import.meta.url));
+    app.use('/', express.static(join(directoryName, './static')));
     app.use('*', assignRequestId);
     
     app.get('/api', (req, res) => res.sendStatus(301));
